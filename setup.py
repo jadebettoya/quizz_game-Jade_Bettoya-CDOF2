eb264dd99964785e9e87221f8a83b117cc5331dd
jadebettoya <104717442+jadebettoya@users.noreply.github.com>
2024-02-01 20:38:32 +0000

    setup creation in python

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='quiz-game',
+    version='1.0',
+    author='jadebettoya',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
